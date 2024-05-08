6870bf2e1c6dfe17ffc3972efd29cdf76d9c73b5
youssef ahmed <84735296+Youssef548@users.noreply.github.com>
2024-05-08 06:30:53 +0300

    navigate on click on Logo

--- client/src/components/layouts/Navbar/Logo.tsx
+++ client/src/components/layouts/Navbar/Logo.tsx
@@ -1,9 +1,16 @@
 import Image from "../../../utils/Image";
 import svg from "../../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+
 const Logo = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <Image
+        onClick={() => {
+          navigate("/");
+        }}
         src={svg}
         className="hidden md:block cursor-pointer"
         width="100"
