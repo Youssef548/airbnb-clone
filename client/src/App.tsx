024e7698b24474fc5aedae8f9a943632db409095
youssef ahmed <84735296+Youssef548@users.noreply.github.com>
2024-05-08 06:32:46 +0300

    remove some comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import favicon from "./assets/free-airbnb-1869032-1583156.webp"; // Import your favicon file
 import ToasterProvider from "./providers/ToasterProvider";
-import UserProvider from "./providers/UserProvider" ;
+import UserProvider from "./providers/UserProvider";
 const App = () => {
   return (
     <UserProvider>
@@ -17,20 +17,11 @@ const App = () => {
             <meta name="keywords" content="React, Component, Meta Data" />
             <link rel="icon" type="image/webp" href={favicon} />
           </Helmet>
-          {/* Define your layout components here */}
-          {/* For example: <Header /> */}
-
-          {/* Define your routes using the Switch component */}
           <Routes>
-            {/* Route for the Home page */}
             <Route path="/" element={<Home />} />
 
-            {/* Route for a 404 Not Found page */}
             <Route path="/*" element={<NotFound />} />
           </Routes>
-
-          {/* Other layout components like Footer can go here */}
-          {/* For example: <Footer /> */}
         </div>
       </Router>
     </UserProvider>
