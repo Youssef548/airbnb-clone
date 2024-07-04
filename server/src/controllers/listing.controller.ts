cb4d0ab2d008e723195a7cc9a36e3bb9883c6640
youssef ahmed <84735296+Youssef548@users.noreply.github.com>
2024-07-04 20:57:52 +0300

    feat: Update listing model and controller
    getListing by id

--- server/src/controllers/listing.controller.ts
+++ server/src/controllers/listing.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Listing } from "../models/listing.model";
+import { IListing } from "../models/listing.model";
 import { errorHandler } from "../utils/error";
 interface CustomRequest extends Request {
   user: {
@@ -35,7 +36,7 @@ export async function createListing(
       guestCount,
       location: location.value,
       price,
-      userId: request.user.userId,
+      user: request.user.userId,
     });
     await listing.save();
     res.status(201).json(listing);
@@ -57,4 +58,36 @@ export async function getListings(
     console.error(error);
     return next(errorHandler(401, "something went wrong"));
   }
+}
+
+export async function getListingById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { listingId } = req.params;
+    if (!listingId) {
+      return next(errorHandler(400, "Missing listing id"));
+    }
+
+    const listing = await Listing.findById(listingId).populate("user").exec();
+
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+
+    const listingData: IListing = listing.toObject();
+
+    res.status(200).json({
+      ...listingData,
+      createdAt: listingData.createdAt?.toISOString(),
+      user: {
+        ...listingData.user,
+        password: null,
+        createdAt: listingData.createdAt?.toISOString(),
+        updatedAt: listingData.updatedAt?.toISOString(),
+      },
+    });
+  } catch (err) {}
 }
--- server/src/models/listing.model.ts
+++ server/src/models/listing.model.ts
@@ -1,20 +1,45 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-const ListingSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  imageSrc: String,
-  category: String,
-  roomCount: Number,
-  bathRoomCount: Number,
-  guestCount: Number,
-  price: Number,
-  location: String,
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
-  bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Booking" }],
-});
+// Define the interface for the listing
+export interface IListing extends Document {
+  title?: string;
+  description?: string;
+  imageSrc?: string;
+  category?: string;
+  roomCount?: number;
+  bathRoomCount?: number;
+  guestCount?: number;
+  price?: number;
+  location?: string;
+  user: mongoose.Schema.Types.ObjectId;
+  reviews?: mongoose.Schema.Types.ObjectId[];
+  bookings?: mongoose.Schema.Types.ObjectId[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
 
-const ListingModel = mongoose.model("Listing", ListingSchema);
+// Define the schema for the listing
+const ListingSchema: Schema<IListing> = new mongoose.Schema(
+  {
+    title: { type: String },
+    description: { type: String },
+    imageSrc: { type: String },
+    category: { type: String },
+    roomCount: { type: Number },
+    bathRoomCount: { type: Number },
+    guestCount: { type: Number },
+    price: { type: Number },
+    location: { type: String },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
+    bookings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Booking" }],
+  },
+  {
+    timestamps: true, // This automatically adds createdAt and updatedAt
+  }
+);
+
+// Define the model for the listing
+const ListingModel = mongoose.model<IListing>("Listing", ListingSchema);
 
 export { ListingModel as Listing };
