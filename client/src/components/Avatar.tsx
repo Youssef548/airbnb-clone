61deb21de23c8b7bbfa4c7497f9fdbbfa7fef10e
youssef ahmed <84735296+Youssef548@users.noreply.github.com>
2024-07-04 23:18:54 +0300

    refactor: Update Avatar component to accept optional image source prop

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -1,13 +1,17 @@
 import Image from "../utils/Image";
 import svg from "../assets/placeholder.jpg";
-const Avatar = () => {
+
+interface ImageProps {
+  src?: string; // Optional prop for an image source, default to placeholder.jpg if not provided.
+}
+const Avatar = ({ src }: ImageProps) => {
   return (
     <Image
       className="rounded-full"
       width="30"
       height="30"
       alt="Avatar"
-      src={svg}
+      src={src || svg}
     />
   );
 };
