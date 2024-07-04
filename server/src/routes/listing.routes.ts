4901ac33cbacc8979e9ae31ba9f53acb8699f145
youssef ahmed <84735296+Youssef548@users.noreply.github.com>
2024-07-04 23:16:43 +0300

    update: Add getListingById API endpoint

--- server/src/routes/listing.routes.ts
+++ server/src/routes/listing.routes.ts
@@ -1,11 +1,15 @@
-import {Router} from "express";
-import { createListing, getListings } from "../controllers/listing.controller";
+import { Router } from "express";
+import {
+  createListing,
+  getListingById,
+  getListings,
+} from "../controllers/listing.controller";
 import { isAuth } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/create", isAuth, createListing);
-router.get("/all",  getListings);
-
+router.get("/all", getListings);
+router.get("/get/:listingId", isAuth, getListingById);
 
 export default router;
