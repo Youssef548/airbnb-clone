caa10f39f0bfe1d97d0d329b2a738e95b86b6e3d
Youssef548 <youssefahmedpvp5@gmail.com>
2024-07-12 21:00:13 +0300

    refactor: Update ReservationPostType and ReservationsGetType in reservations.types.ts

--- client/src/apis/Reservations/reservations.types.ts
+++ client/src/apis/Reservations/reservations.types.ts
@@ -1,6 +1,12 @@
 export type ReservationPostType = {
-    totalPrice: number;
-    startDate: Date;
-    endDate: Date;
-    listingId: string;
-}
+  totalPrice: number;
+  startDate: Date;
+  endDate: Date;
+  listingId: string;
+};
+
+export type ReservationsGetType = {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+};
