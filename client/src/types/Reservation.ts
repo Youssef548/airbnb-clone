b63460f7af486468ae41759f08f990558d02b414
Youssef548 <youssefahmedpvp5@gmail.com>
2024-07-12 20:59:25 +0300

    refactor: Update ReservationType and ReservationSafeType in Reservation.ts

--- client/src/types/Reservation.ts
+++ client/src/types/Reservation.ts
@@ -1,8 +1,20 @@
+import { ListingType } from "./Listing";
+
 export type ReservationType = {
-    listingId: string; // Assuming listingId is represented as a string
-    userId: string; // Assuming userId is represented as a string
-    checkInDate: Date;
-    checkOutDate: Date;
-    status: 'confirmed' | 'pending' | 'cancelled';
-    totalPrice: Number; 
-}
+  listingId: string; // Assuming listingId is represented as a string
+  userId: string; // Assuming userId is represented as a string
+  checkInDate: Date;
+  checkOutDate: Date;
+  status: "confirmed" | "pending" | "cancelled";
+  totalPrice: Number;
+};
+
+export type ReservationSafeType = Omit<
+  ReservationType,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: ListingType;
+};
