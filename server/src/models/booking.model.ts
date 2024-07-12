b30696b1e8c4d5a9781183bbe14f67ec7fc00659
Youssef548 <youssefahmedpvp5@gmail.com>
2024-07-12 20:57:29 +0300

    refactor: Update Booking model to include createdAt and updatedAt fields

--- server/src/models/booking.model.ts
+++ server/src/models/booking.model.ts
@@ -1,20 +1,35 @@
+import { timeStamp } from "console";
 import mongoose from "mongoose";
+import { IListing } from "./listing.model";
 
 export interface IBooking extends Document {
   startDate?: Date;
   endDate?: Date;
   guest?: mongoose.Schema.Types.ObjectId;
-  listing?: mongoose.Schema.Types.ObjectId;
+  listing?: IListing;
+  listingId?: IListing;
   totalPrice: Number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const BookingSchema = new mongoose.Schema({
-  startDate: Date,
-  endDate: Date,
-  guest: { type: mongoose.Types.ObjectId, ref: "User" },
-  listingId: { type: mongoose.Types.ObjectId, ref: "Listing" },
-  totalPrice: Number,
-});
+const BookingSchema = new mongoose.Schema(
+  {
+    startDate: Date,
+    endDate: Date,
+    guest: { type: mongoose.Types.ObjectId, ref: "User" },
+    listingId: { type: mongoose.Types.ObjectId, ref: "Listing" },
+    authorId: { type: mongoose.Types.ObjectId, ref: "User" },
+    totalPrice: Number,
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+      required: true,
+    },
+  }
+);
 
 const BookingModel = mongoose.model<IBooking>("Booking", BookingSchema);
 
