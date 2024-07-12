4dfec7c498554b427b6413dccc7cbfd111cbe5e1
Youssef548 <youssefahmedpvp5@gmail.com>
2024-07-12 23:35:20 +0300

    refactor: Remove commented out code in Layout.tsx

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -19,8 +19,6 @@ function Layout() {
           <Navbar user={user} />
           <Outlet />
         </Suspense>
-
-        {user && <div>Welcome, {user.username}!</div>}
       </div>
     </>
   );
