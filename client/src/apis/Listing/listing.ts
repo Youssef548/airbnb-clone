6709ae3138318abc91a7a55c82a6f90114cfffcd
Youssef548 <youssefahmedpvp@gmail.com>
2024-07-14 22:03:41 +0300

    refactor: Add search functionality to getListing API endpoint

--- client/src/apis/Listing/listing.ts
+++ client/src/apis/Listing/listing.ts
@@ -1,9 +1,15 @@
 import { axiosInstance } from "../../providers/AxiosInstance";
-export  function getListing() {
-    return axiosInstance.get(`/listings/all`)
+import { IListingParams } from "./listing.types";
+export function getListing(searchParams: IListingParams) {
+  return axiosInstance.get(`/listings/all`, {
+    params: { ...searchParams },
+  });
 }
 
 export function getListingById(id: string) {
-    return axiosInstance.get(`/listings/get/${id}`)
+  return axiosInstance.get(`/listings/get/${id}`);
 }
 
+export function deleteListing(id: string) {
+  return axiosInstance.delete(`/listings/delete/${id}`);
+}
