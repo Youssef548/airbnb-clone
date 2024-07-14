24873fc2ea7912f278881f427faef2a277acab5f
Youssef548 <youssefahmedpvp@gmail.com>
2024-07-14 17:13:28 +0300

    refactor: Update ListingType to include createdAt property

--- client/src/types/Listing.ts
+++ client/src/types/Listing.ts
@@ -1,17 +1,21 @@
 import { UserType } from "./user";
 
 export type ListingType = {
-    _id: string;
-    title: string;
-    description: string;
-    imageSrc: string;
-    category: string;
-    roomCount: number;
-    bathRoomCount: number;
-    guestCount: number;
-    price: number;
-    location: string;
-    user: UserType; 
-    reviews: string[]; 
-    bookings: string[]; 
-}
+  _id: string;
+  title: string;
+  description: string;
+  imageSrc: string;
+  category: string;
+  roomCount: number;
+  bathRoomCount: number;
+  guestCount: number;
+  price: number;
+  location: string;
+  user: UserType;
+  reviews: string[];
+  bookings: string[];
+};
+
+export type safeListingType = Omit<ListingType, "createdAt"> & {
+  createdAt: string;
+};
