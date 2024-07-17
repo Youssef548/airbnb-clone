890f15346c6ef65c8e6640c7368072612663383f
Youssef548 <youssefahmedpvp@gmail.com>
2024-07-18 02:58:30 +0300

    feat: Add SearchModal component for advanced search functionality

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -6,6 +6,7 @@ import RegisterModal from "./Modals/RegisterModal";
 import LoginModal from "./Modals/LoginModal";
 import useUserStore from "../store/useStore";
 import RentModal from "./Modals/RentModal";
+import SearchModal from "./Modals/SearchModal";
 function Layout() {
   const user = useUserStore((state) => state.user);
 
@@ -13,6 +14,7 @@ function Layout() {
     <>
       <div>
         <Suspense fallback={<div>Loading...</div>}>
+          <SearchModal />
           <LoginModal />
           <RentModal />
           <RegisterModal />
