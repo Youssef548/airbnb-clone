39932926f80120fc616f0efd2702c7ccaf84928b
Youssef548 <youssefahmedpvp@gmail.com>
2024-07-18 02:57:01 +0300

    feat: Add new fields to IListingParams interface for advanced search functionality

--- client/src/apis/Listing/listing.types.ts
+++ client/src/apis/Listing/listing.types.ts
@@ -1,3 +1,9 @@
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathRoomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
 }
