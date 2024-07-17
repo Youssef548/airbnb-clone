5941d567e589514e30710d24f53b638a0abb5ca4
Youssef548 <youssefahmedpvp@gmail.com>
2024-07-18 02:58:43 +0300

    refactor: Update CategoryBox component to fetch listings on category selection

--- client/src/components/CategoryBox.tsx
+++ client/src/components/CategoryBox.tsx
@@ -1,13 +1,15 @@
-import React, { useCallback } from 'react';
-import { Icon, IconifyIcon } from '@iconify/react';
-import {  useSearchParams } from 'react-router-dom';
-import queryString from 'qs';
+import React, { useCallback } from "react";
+import { Icon, IconifyIcon } from "@iconify/react";
+import { useSearchParams } from "react-router-dom";
+import queryString from "qs";
+import { getListing } from "../apis/Listing/listing";
+import ListingStore from "../store/listingsStore";
 
 interface CategoryBoxProps {
   label: string;
-  description?: string; 
+  description?: string;
   selected?: boolean;
-  icon: IconifyIcon; 
+  icon: IconifyIcon;
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({
@@ -16,6 +18,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   icon,
   selected = false, // Default value for selected
 }) => {
+  const { setListings } = ListingStore();
   let [params, setSearchParams] = useSearchParams();
 
   let qs = queryString as any;
@@ -32,22 +35,29 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
       category: label,
     };
 
-    if (params?.get('category')  == label )  {
+    if (params?.get("category") == label) {
       delete updatedQuery.category;
     }
 
-    setSearchParams(updatedQuery)
+    setSearchParams(updatedQuery);
 
+    getListing({ ...updatedQuery }).then((res) => {
+      if (res.status === 200) {
+        setListings(res.data);
+      }
+    });
   }, [params, label]);
 
   return (
     <div
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
-        selected ? 'border-b-neutral-800 text-neutral-800' : 'border-transparent text-neutral-500'
+        selected
+          ? "border-b-neutral-800 text-neutral-800"
+          : "border-transparent text-neutral-500"
       }`}
       onClick={handleClick}
     >
-      <Icon icon={icon} style={{ fontSize: '26px' }} />
+      <Icon icon={icon} style={{ fontSize: "26px" }} />
       <div>{label}</div>
       {description && <p className="text-sm">{description}</p>}
     </div>
