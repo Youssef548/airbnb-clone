c8f127d0f04e1e2266cd7f46cd0cccac6225ef2e
Youssef548 <youssefahmedpvp@gmail.com>
2024-07-25 19:44:19 +0300

    refactor: Improve search functionality in Navbar component

--- client/src/components/layouts/Navbar/Search.tsx
+++ client/src/components/layouts/Navbar/Search.tsx
@@ -1,7 +1,45 @@
+import { useSearchParams } from "react-router-dom";
 import useSearchModal from "../../../hooks/useSearchModal";
+import useCountries from "../../../hooks/useCountries";
+import { useMemo } from "react";
+import { differenceInDays } from "date-fns";
 
 const Search = () => {
   const searchModal = useSearchModal();
+  let [params] = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get("locationValue");
+  const startDate = params.get("startDate");
+  const endDate = params.get("endDate");
+  const guestCount = params.get("guestCount");
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return "anywhere";
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) diff = 1;
+      return `${diff} Days`;
+    }
+
+    return "Any week";
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) return `${guestCount} Guests`;
+
+    return "Add Guests";
+  }, [guestCount]);
+
   return (
     <div
       onClick={searchModal.onOpen}
@@ -32,7 +70,7 @@ const Search = () => {
     px-6
     "
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div
           className="
@@ -46,10 +84,10 @@ const Search = () => {
         text-center
         "
         >
-          Any Week
+          {durationLabel}
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div className="p-2 bg-rose-500 rounded-full text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
