c983063af4e54d78744860cabd91b0d3e9c71b65
Youssef548 <youssefahmedpvp@gmail.com>
2025-02-25 23:03:40 +0200

    fix(models): Make username and email fields required in User model schema

--- server/src/models/User.model.ts
+++ server/src/models/User.model.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String },
-    email: { type: String, unique: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     emailVerified: { type: Date },
     image: { type: String },
     password: String,
