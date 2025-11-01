a37b11a3beac4f0b28f0569bc0f53f38a9a5ba28
Youssef548 <youssefahmedpvp@gmail.com>
2025-11-01 10:44:48 +0200

    test: verify Husky pre-commit hook formatting

---
+++ test-formatting.js
@@ -0,0 +1,9 @@
+// This file has intentionally bad formatting to test Husky
+const   badlyFormatted=   {
+  foo:    "bar"  ,
+    baz   :   "qux"
+}
+
+function test(  ){
+      console.log(  "This should be formatted"  )
+}
